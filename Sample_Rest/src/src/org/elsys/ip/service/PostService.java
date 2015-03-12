3aa80135de4d070713766c922ae451abab92afa1
asenisa12 <asenisa@abv.bg>
2015-03-12 18:26:32 +0200

    Service Template added

---
+++ Sample_Rest/src/src/org/elsys/ip/service/PostService.java
@@ -0,0 +1,25 @@
+package src.org.elsys.ip.service;
+
+import java.util.List;
+
+import src.org.elsys.ip.model.Post;
+
+public class PostService {
+
+	public List<Post> getPosts() {
+		return null;
+	}
+	public Post getPost(long postId) {
+		return null;
+	}
+	public Post createPost(Post post) {
+		return null;
+	}
+	public Post updatePost(Post post) {
+		return null;
+	}
+	public void deletePost(long postId) {
+		
+	}
+
+}
