b8fedd1509c90ed98813eaf1b5754166792c31dd
asenisa12 <asenisa@abv.bg>
2015-03-12 18:44:17 +0200

    Endpoint template Added

---
+++ Sample_Rest/src/src/org/elsys/ip/rest/PostRest.java
@@ -0,0 +1,24 @@
+package src.org.elsys.ip.rest;
+
+import java.util.List;
+
+import src.org.elsys.ip.model.Post;
+
+public class PostsRest {
+
+	public List<Post> getPosts() {
+		return null;
+	}
+	public Post getPost(long postId) {
+		return null;
+	}
+	public Post createPost(Post post) {
+		return null;
+	}
+	public Post updatePost(Post post) {
+		return null;
+	}
+	public void deletePost(long postId) {
+		
+	}
+}
